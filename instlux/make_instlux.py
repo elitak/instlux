da15d216c9df05b28b57c0b81f5d1af8dd01e8c1
jordi_massaguer <jordi_massaguer@fbba4e05-0f2c-0410-b72c-c723cb084968>
2007-03-20 21:36:41 +0000

    Initial import.
    git-svn-id: http://svn.code.sf.net/p/instlux/code@2 fbba4e05-0f2c-0410-b72c-c723cb084968

---
+++ instlux/make_instlux.py
@@ -0,0 +1,177 @@
+import os
+import sys
+
+#kernels = [ {"distribution":"Linkat","version":"1.0","media":"CDROM","kernel":"LINUX2","drivers":"INITRD2","append":"root=/dev/hdc devfs=mount,dall ramdisk_size=65536"}]
+kernels = [ {"distribution":"OpenSuSE","version":"10.2","media":"NET","kernel":"linux","drivers":"initrd","append_ide":"devfs=mount,dall ramdisk_size=65536 install=http:opensuse/distribution/10.2/repo/oss server=155.210.39.56","append_sata":"devfs=mount,dall ramdisk_size=65536 install=http:opensuse/distribution/10.2/repo/oss server=155.210.39.56"}]
+languages = os.listdir("translations")
+#languages = ["english"]
+
+#2007-01-07
+list_of_contributors = "Greg Johnston\nMichael\nMarc Herbert\nPiarres beobide\nsesammases\nbalu_kalla\nnumatrix\nnotable\nJimmyGoon\nfra1027\nrev pete moss\nRajesh\nHeath\nAMCDeathKnight\nRobin Patt-Corner\ncarsten\nTonA\ntomtenberge\nJack Chen\nKevin\nTim\nDaveW\nBhasadu\nMarcoAurelio\nEl Paco\nkkkkk\nDan\nAas\nmetallicgreenb\nGrymyrk\nATB\nEmanuel Levy\nindygo\npetrbok\nVati-Khan\noffdutyBorg\nalicia_sb\nSam Johnston\nHarryhe\ntotro2\nHenrik Brink"
+
+build = "build"
+nsis_bin = '"c:\Program Files\NSIS\makensis.exe"'
+instlux_ico = "instlux.ico"
+instlux_logo = "instlux_logo.bmp"
+grub4dos = "grldr"
+
+def copy_from_src_to_build( build, file ):
+	listdir = os.listdir( build )
+	if file not in listdir:
+		input = open("src"+os.sep+file, "rb")
+		output = open( build+os.sep+file, "wb")
+		output.write( input.read() )
+		output.flush()
+		output.close()
+		input.close()
+
+def create_build_dirs( build, languages, instlux_ico, instlux_logo, grub4dos):
+    listdirs = os.listdir(".")
+    if "bin" not in listdirs:
+        os.mkdir( "bin" )
+    listdirs = os.listdir(".")
+    if build not in listdirs:
+        os.mkdir( build )
+
+    listdirs = os.listdir( build )
+    if "translations" not in listdirs:
+        os.mkdir( build+os.sep+"translations" )
+
+    if "bin" not in listdirs:
+        os.mkdir( build+os.sep+"bin" )
+
+    listdirs = os.listdir( build+os.sep+"translations" )
+    for language in languages:
+        if language not in listdirs:
+            os.mkdir( build+os.sep+"translations"+os.sep+language )
+	copy_from_src_to_build( build, instlux_ico )
+   	copy_from_src_to_build( build, instlux_logo )
+   	copy_from_src_to_build( build, grub4dos )
+  
+
+def get_linuxes( kernels ):
+	linuxes=[];
+	for kernel in kernels:
+		version = kernel["version"].replace(".","_")
+		name = kernel["media"]+kernel["distribution"]+version+"_en"
+		linuxes = linuxes+[name.replace("_en","")]
+	return linuxes;
+
+def get_customizations( kernels, build):
+	customizations = []
+	for kernel in kernels:
+		version = kernel["version"].replace(".","_")
+		name = "instlux"+kernel["media"]+kernel["distribution"]+version+"_en"
+		caption = kernel["distribution"]+" "+kernel["version"]+" installer"
+		list_of_files_string =""
+		dir_out = name.replace("_en","")
+		dir = "distros"+os.sep+dir_out
+		append_ide = kernel["append_ide"]
+		append_sata = kernel["append_sata"]
+		for dirpath, dirnames, filenames in os.walk( dir ):
+			dirpath_formated = dirpath.replace("/","\\").replace( build+os.sep,"")
+			list_of_files_string = list_of_files_string+"   SetOutPath $INSTDIR\\"+dirpath_formated+"\n"
+			for file in filenames:
+				list_of_files_string = list_of_files_string+"   File \"..\\"+dirpath_formated+"\\"+file+"\"\n"
+		customizations.append({"FILENAME":name+".nsi","NAME":name,"OUTFILE":name+".exe", "CAPTION":caption, "MENU_TITLE":caption, "KERNEL":dir_out+"/"+kernel["kernel"], "DRIVERS":dir_out+"/"+kernel["drivers"], "LIST_OF_FILES":list_of_files_string, "BOOT_TITLE":caption, "OUTPATH":dir_out, "APPEND_IDE":append_ide, "APPEND_SATA":append_sata})
+	return customizations;
+
+def create_one_nsis_file_for_distro( customizations,  build ):
+    template_file = open( "src"+os.sep+"instlux_template_en.nsi", "r" )
+    orig_data = template_file.read()
+    template_file.close()
+
+    for item in customizations:
+	data = orig_data
+	f = open(  build+os.sep+item["FILENAME"], "w" )
+	for customization_key, customization_value in item.iteritems():
+		data = data.replace( customization_key, customization_value )
+
+	f.write( data )
+	f.flush()
+	f.close()
+
+
+def get_translations( languages, linuxes):
+	translations = {}
+	for language in languages:
+		if not translations.has_key( language ):
+			translations[ language ] = []
+		for linux in linuxes:
+			translations[ language ].append([linux+"_en",linux+language])
+			translations[ language ].append(["English",language])
+			translations[ language ].append(["license_en.txt","translations\\"+language+"\\license_"+language+".txt"])
+	for language in languages:
+		try:
+			filename = "translations"+os.sep+language+os.sep+"translations_"+language+".txt"
+			f = open( filename, "r")
+			print language+" translations"
+			for line in f.readlines():
+				double_quotes_pos = line.find( "\"" )
+				if double_quotes_pos != -1:
+					double_quotes_pos_next = line[double_quotes_pos+1:].find( "\"" )
+					if double_quotes_pos_next != -1:
+						double_quotes_pos_next = double_quotes_pos_next+double_quotes_pos+2
+						key = line[double_quotes_pos:double_quotes_pos_next]
+						double_quotes_pos = line[double_quotes_pos_next+1:].find( "\"")
+						if double_quotes_pos != -1:
+							double_quotes_pos = double_quotes_pos_next+1+double_quotes_pos
+							double_quotes_pos_next = line[double_quotes_pos+1:].find( "\"")
+							if double_quotes_pos_next != -1:
+								double_quotes_pos_next = double_quotes_pos_next+double_quotes_pos+2
+								value = line[double_quotes_pos:double_quotes_pos_next]
+								translations[ language ].append( [key, value] )
+		except:
+			print "warning: translations for "+language+" not found in "+filename
+	return translations;
+
+def translate(input_file_name, output_file_name, translations):
+    input_file=open( input_file_name, "r")
+    output_file=open( output_file_name, "w")
+    for line in input_file.readlines():
+        data = line
+        for translation in translations:
+            if line.find(translation[0])!=-1:
+                data = line.replace(translation[0], translation[1])
+        output_file.write( data )
+
+    output_file.flush()
+    output_file.close()
+    input_file.close()
+
+def create_all_nsis( translations, linuxes, build):
+	for language,values in translations.iteritems():
+		for linux in linuxes:
+			input_file_name = build+os.sep+"instlux"+linux+"_en.nsi"
+			output_file_name = build+os.sep+"instlux"+linux+"_"+language+".nsi"
+			translate( input_file_name, output_file_name, values)
+			
+def compile_nsis( translations, linuxes, build, nsis_bin):
+	for language,values in translations.iteritems():
+		for linux in linuxes:
+			file_name = build+os.sep+"instlux"+linux+"_"+language+".nsi"
+			command = nsis_bin+" "+file_name
+			os.system( command )
+
+def create_translated_licenses ( languages, build, list_of_contributors):
+	for language in languages:
+		f_orig = open( "translations"+os.sep+language+os.sep+"license_"+language+".txt", "r")
+		f_dest = open( build+os.sep+"translations"+os.sep+language+os.sep+"license_"+language+".txt", "w")
+		f_dest.write( f_orig.read().replace( "LIST_OF_CONTRIBUTORS", list_of_contributors ) )
+		f_dest.flush()
+		f_dest.close()
+		f_orig.close()
+
+
+create_build_dirs(  build, languages, instlux_ico, instlux_logo, grub4dos )
+linuxes = get_linuxes( kernels )
+customizations = get_customizations( kernels, build)
+translations = get_translations( languages, linuxes );
+create_one_nsis_file_for_distro( customizations, build )
+create_translated_licenses ( languages, build, list_of_contributors)
+create_all_nsis( translations, linuxes, build)
+compile_nsis( translations, linuxes, build, nsis_bin)
+
+
+
+
---
+++ instlux/src/instlux_template_en.nsi
@@ -0,0 +1,154 @@
+; filename: instlux.nsi
+;
+; Linux installer: Install linux boots from you hard disk, so that
+;                  the system bios does not need to be changed.
+;                  You can choose from different linux installers (different
+;                  distributions and different media).
+;                  For network installations, program sources will be downloaded
+;                  through internet during the install process, so internet 
+;                  connection should be provided.
+;                  For CD installations, program sources will be downloaded
+;                  from a CD, so that you should have a CD. That CD could be
+;                  obtained from internet, magazines, ...
+;
+;                  In order to achieve that, grub for DOS is installed
+;                  into your hard disc, as well as the needed kernel and drivers.
+;                  Then, boot.ini is changed to include grub on the boot process.
+;                  After that, the system will be rebooted so that you can install
+;                  linux. Next time you boot windows, the boot.ini will be restored.
+;
+;
+;    Copyright (C) 2005  Jordi Massaguer Pla
+
+;    This program is free software; you can redistribute it and/or modify
+;    it under the terms of the GNU General Public License as published by
+;    the Free Software Foundation; either version 2 of the License, or
+;    (at your option) any later version.
+;
+;    This program is distributed in the hope that it will be useful,
+;    but WITHOUT ANY WARRANTY; without even the implied warranty of
+;    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;    GNU General Public License for more details.
+;
+;    You should have received a copy of the GNU General Public License
+;    along with this program; if not, write to the Free Software
+;    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+;
+; 
+;--------------------------------
+;Include Modern UI
+
+  !include "MUI.nsh"
+
+;--------------------------------
+;General
+
+  Name "NAME"
+  AllowRootDirInstall true
+  OutFile "..\bin\OUTFILE"
+  Caption "CAPTION"
+
+  ;Default installation folder
+  !include LogicLib.nsh
+  !include FileFunc.nsh ; GetRoot / un.GetRoot
+  !insertmacro GetRoot
+  !insertmacro un.GetRoot
+  BGGradient f8e409  
+
+  !define MUI_ICON "instlux.ico"
+  !define MUI_UNICON "instlux.ico"
+  !define MUI_HEADERIMAGE
+  !define MUI_HEADERIMAGE_BITMAP "instlux_logo.bmp"
+
+  !define MUI_WELCOMEPAGE_TITLE_3LINES 
+  !define MUI_FINISHPAGE_TITLE_3LINES
+  
+
+;--------------------------------
+;Interface Settings
+
+  !define MUI_ABORTWARNING
+
+;--------------------------------
+;Pages
+
+  !insertmacro MUI_PAGE_WELCOME
+  !insertmacro MUI_PAGE_LICENSE "license_en.txt"
+  !insertmacro MUI_PAGE_INSTFILES
+  !insertmacro MUI_PAGE_FINISH    
+  !insertmacro MUI_UNPAGE_CONFIRM
+  !insertmacro MUI_UNPAGE_INSTFILES
+  
+;--------------------------------
+;Languages
+ 
+  !insertmacro MUI_LANGUAGE "English"
+
+;--------------------------------
+; splash
+
+ XPStyle on
+ ;icon instlux_logo_icon2.ico
+var /GLOBAL c
+Function .onInit
+	# the plugins dir is automatically deleted when the installer exits
+	InitPluginsDir
+	${GetRoot} $WINDIR $c
+	StrCpy $INSTDIR "$c"
+	File /oname=$PLUGINSDIR\splash.bmp "instlux_logo.bmp"
+	#optional
+	#File /oname=$PLUGINSDIR\splash.wav "C:\myprog\sound.wav"
+
+	splash::show 1000 $PLUGINSDIR\splash
+
+	Pop $0 ; $0 has '1' if the user closed the splash screen early,
+			; '0' if everything closed normally, and '-1' if some error occurred.
+FunctionEnd
+
+InstallDir "$c\"
+
+;-------------------------------
+
+;Sections
+
+Section ""
+  var /GLOBAL MenuLSTFile
+  SetOutPath $INSTDIR
+  FileOpen $MenuLSTFile $c\menu.lst w
+  FileWrite $MenuLSTFile "title MENU_TITLE $\r$\n"
+  FileWrite $MenuLSTFile "kernel   (hd0,0)/distros/KERNEL APPEND_IDE$\r$\n"
+  FileWrite $MenuLSTFile "initrd   (hd0,0)/distros/DRIVERS$\r$\n"
+  ;FileWrite $MenuLSTFile "title MENU_TITLE (SATA or SCSI)$\r$\n"
+  ;FileWrite $MenuLSTFile "kernel   (hd0,0)/distros/KERNEL APPEND_SATA$\r$\n"
+  ;FileWrite $MenuLSTFile "initrd   (hd0,0)/distros/DRIVERS$\r$\n"
+  FileSeek $MenuLSTFile 0 END
+  FileClose $MenuLSTFile
+  File "grldr"
+  LIST_OF_FILES
+  SetFileAttributes "$c\boot.ini" NORMAL
+  SetFileAttributes "$c\boot.ini" SYSTEM|HIDDEN
+  WriteINIStr $c\boot.ini "operating systems" "$c\grldr" '"BOOT_TITLE"' 
+  WriteUninstaller "$SMSTARTUP\instlux-uninst.exe"
+  SetRebootFlag true
+
+SectionEnd
+
+;-------------------------------------------
+; uninstall section
+
+;Uninstaller
+
+Section "Uninstall"
+  ${un.GetRoot} $WINDIR $c
+
+  Delete /REBOOTOK "$c\menu.lst"
+  Delete /REBOOTOK "$c\grldr"
+  RMDir /REBOOTOK /r "$c\distros\OUTPATH"
+  SetFileAttributes "$c\boot.ini" NORMAL
+  SetFileAttributes "$c\boot.ini" SYSTEM|HIDDEN
+  DeleteINIStr $c\boot.ini "operating systems" "$c\grldr"
+  Delete /REBOOTOK "$SMSTARTUP\instlux-uninst.exe"
+  
+SectionEnd
+
+
